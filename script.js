75327d00c14bd131c776f1e5ac20b4edb171e031
Akash Ramanand Rajak <aakashrajak02@gmail.com>
2023-06-08 03:23:13 +0530

    Added the profile picture and link to the github account

--- script.js
+++ script.js
@@ -1,7 +1,87 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//   const repoInfo = document.getElementById("repository-info");
+//   const owner = "akash-rajak";
+//   const apiUrl = `https://api.github.com/users/${owner}/repos`;
+
+//   const fetchOwnerDetails = async () => {
+//     const response = await fetch(`https://api.github.com/users/${owner}`);
+//     const data = await response.json();
+//     return data;
+//   };
+
+//   const fetchAllRepositories = async () => {
+//     let allRepos = [];
+//     let page = 1;
+//     let reposPerPage = 500;
+
+//     while (true) {
+//       const url = `${apiUrl}?per_page=${reposPerPage}&page=${page}`;
+//       const response = await fetch(url);
+//       const repos = await response.json();
+
+//       if (repos.length === 0) {
+//         break;
+//       }
+
+//       allRepos = allRepos.concat(repos);
+//       page++;
+//     }
+
+//     return allRepos;
+//   };
+
+//   Promise.all([fetchOwnerDetails(), fetchAllRepositories()])
+//     .then(([ownerData, repos]) => {
+//       const filteredRepos = repos.filter(repo => !repo.fork); // Exclude forked repositories
+
+//       // Sort repositories by stars in descending order
+//       const sortedReposByStars = filteredRepos.sort((a, b) => b.stargazers_count - a.stargazers_count);
+
+//       // Sort repositories by forks in descending order
+//       const sortedReposByForks = filteredRepos.sort((a, b) => b.forks_count - a.forks_count);
+
+//       const ownerName = ownerData.login; // Get owner's login name
+//       const ownerAvatar = ownerData.avatar_url; // Get owner's avatar URL
+
+//       const profileInfo = document.getElementById("profile-info");
+//       const profilePicture = document.getElementById("profile-picture");
+//       const ownerNameElement = document.getElementById("owner-name");
+
+//       profilePicture.src = ownerAvatar;
+//       ownerNameElement.textContent = ownerName;
+
+//       let stats = '';
+//       sortedReposByStars.forEach(repo => {
+//         stats += `
+//           <div class="repo-stats">
+//             <ul>
+//               <li><h3><a href="${repo.html_url}" target="_blank">${repo.name}</a></h3></li>
+//               <li><strong>Description:</strong> ${repo.description}</li>
+//               <li><strong>Stars:</strong> ${repo.stargazers_count}</li>
+//               <li><strong>Forks:</strong> ${repo.forks_count}</li>
+//             </ul>
+//           </div>
+//         `;
+//       });
+//       repoInfo.innerHTML += stats;
+//     })
+//     .catch(error => {
+//       repoInfo.textContent = "Error fetching repository statistics.";
+//       console.error(error);
+//     });
+// });
+
+
 document.addEventListener("DOMContentLoaded", function() {
   const repoInfo = document.getElementById("repository-info");
   const owner = "akash-rajak";
   const apiUrl = `https://api.github.com/users/${owner}/repos`;
+
+  const fetchOwnerDetails = async () => {
+    const response = await fetch(`https://api.github.com/users/${owner}`);
+    const data = await response.json();
+    return data;
+  };
 
   const fetchAllRepositories = async () => {
     let allRepos = [];
@@ -24,8 +104,8 @@ document.addEventListener("DOMContentLoaded", function() {
     return allRepos;
   };
 
-  fetchAllRepositories()
-    .then(repos => {
+  Promise.all([fetchOwnerDetails(), fetchAllRepositories()])
+    .then(([ownerData, repos]) => {
       const filteredRepos = repos.filter(repo => !repo.fork); // Exclude forked repositories
 
       // Sort repositories by stars in descending order
@@ -34,13 +114,22 @@ document.addEventListener("DOMContentLoaded", function() {
       // Sort repositories by forks in descending order
       const sortedReposByForks = filteredRepos.sort((a, b) => b.forks_count - a.forks_count);
 
-      const ownerName = owner; // Capitalize owner name
-      const heading = document.createElement("h1");
-      const ownerLink = document.createElement("a"); // Create the anchor element
-      ownerLink.href = `https://github.com/${owner}`; // Set the href attribute to the owner's GitHub profile
-      ownerLink.textContent = ownerName; // Set the link text to the owner name
-      heading.appendChild(ownerLink); // Append the anchor element to the heading
-      repoInfo.appendChild(heading);
+      const ownerName = ownerData.login; // Get owner's login name
+      const ownerAvatar = ownerData.avatar_url; // Get owner's avatar URL
+
+      const profileInfo = document.getElementById("profile-info");
+      const profilePicture = document.getElementById("profile-picture");
+      const ownerNameElement = document.getElementById("owner-name");
+
+      profilePicture.src = ownerAvatar;
+
+      const ownerProfileLink = document.createElement("a");
+      ownerProfileLink.href = `https://github.com/${owner}`;
+      ownerProfileLink.textContent = ownerName;
+      ownerProfileLink.target = "_blank";
+
+      ownerNameElement.innerHTML = ""; // Clear the existing content
+      ownerNameElement.appendChild(ownerProfileLink);
 
       let stats = '';
       sortedReposByStars.forEach(repo => {
